8cd91666a08c506b2483bc6bbe6a9472c14ec0e5
Ahmed Ashraf <ahmedashraf@Ahmeds-iMac-Pro.local>
2022-11-04 14:34:20 +0200

    csrf bugs fixed

--- src/settings.py
+++ src/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -151,7 +151,7 @@ JAZZMIN_SETTINGS = {
     "site_brand": "Golf Cart",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "users/img/logo.jpg",
+    "site_logo": "staticfiles/users/img/logo.jpg",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": None,
